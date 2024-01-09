e6369287fa74f90d04cded2ae3e4c5a9d0eef4f6
ZennZen00 <148758032+ZennZen00@users.noreply.github.com>
2024-01-09 15:49:35 +0700

    Add files via upload

---
+++ news.py
@@ -0,0 +1,38 @@
+import requests
+
+API_KEY = '62d99c5f4126499790c14588724fcd6b'
+
+# URL untuk endpoint yang ingin diakses
+url = 'https://newsapi.org/v2/top-headlines'
+
+parameters = {
+    'country': 'id',  # Ubah kode negara jika Anda ingin berita dari negara lain
+    'category': 'Health', #Kategori berita
+    'language': 'id', #bahasa berita
+    'sortBy': 'relevancy', # Mengurutkan berdasarkan popularitas
+    'pageSize': 20, # Jumlah artikel yang muncul
+    'from': '2023-12-11',  # Ambil artikel mulai dari tanggal tertentu
+    'to': '2024-1-6',  # Ambil artikel hingga tanggal tertentu
+    'apiKey': API_KEY
+    
+}
+
+# Buat GET request
+response = requests.get(url, params=parameters)
+
+# Cek jika request berhasil (status code 200)
+if response.status_code == 200:
+   
+    news_data = response.json()
+    
+    # DTampilkan beberapa informasi dari respons
+    for article in news_data['articles']:
+        print(f"Title: {article['title']}")
+        print(f"Source: {article['source']['name']}")
+        print(f"URL: {article['url']}")
+        print("--------------------------------------------------")
+else:
+    print("Gagal untuk mendapatkan berita. Status code:", response.status_code)
+
+
+
